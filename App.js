51e08d64b9e50c78dbee6012be048fbb72017c95
Niamh <niamh.gowran2@mail.dcu.ie>
2022-12-30 04:02:35 +0000

    Inital

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Root() {
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: "#38639F",
+      tabBarInactiveTintColor: "gray",
+      tabBarStyle: [{ display: "flex" }, null],
+    }}
+  >
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="ios-home" color={color} size={size} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator options={{ headerShown: false }}>
+          <Stack.Screen name="Root" options={{ headerShown: false }} component={Root} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View >
+            <Text >Welcome to Parking Protect!</Text>
+        </View >
+    );
+}
