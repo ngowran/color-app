f8c36bdaaf5baec2f812b0a4bdf5225a006123f6
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-12-30 05:00:21 +0000

    Fixed dependencies, .gitignore update, bottom tab

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import React from "react";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -27,27 +28,48 @@ function Root() {
         ),
       }}
     />
-  </Tab.Navigator>
+  </Tab.Navigator>;
 }
-
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator options={{ headerShown: false }}>
-          <Stack.Screen name="Root" options={{ headerShown: false }} component={Root} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#38639F",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: [{ display: "flex" }, null],
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-home" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-settings" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,9 +2,9 @@ import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
-    return (
-        <View >
-            <Text >Welcome to Parking Protect!</Text>
-        </View >
-    );
+  return (
+    <View>
+      <Text>Welcome to Color App!</Text>
+    </View>
+  );
 }
